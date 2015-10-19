318f9a6e4710f3526e4b64583e952887fac79b8e
klashxx <klashxx@gmail.com>
2015-10-20 00:24:21 +0200

    Methods with pointer receivers

--- methods_interfaces/methods_interfaces.go
+++ methods_interfaces/methods_interfaces.go
@@ -31,10 +31,31 @@ func (f MyFloat) Abs() float64 {
 	return float64(f)
 }
 
+type VertexPointer struct {
+	X, Y float64
+}
+
+func (v *VertexPointer) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *VertexPointer) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
 func main() {
 	v := &Vertex{3, 4}
 	fmt.Println(v.Abs())
 
 	f := MyFloat(-math.Sqrt2)
 	fmt.Println(f.Abs())
+
+	// Methods can be associated with a named type
+	// or a pointer to a named type.
+
+	vp := &VertexPointer{3, 4}
+	fmt.Printf("Before scaling: %+v, Abs: %v\n", vp, vp.Abs())
+	vp.Scale(5)
+	fmt.Printf("After scaling: %+v, Abs: %v\n", vp, vp.Abs())
 }
