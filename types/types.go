3d5608644313ad1b182d5b99d14df63776cf498a
klashxx <klashxx@gmail.com>
2015-10-20 00:07:05 +0200

    function closures

--- types/types.go
+++ types/types.go
@@ -173,9 +173,36 @@ func main() {
 	}
 
 	fmt.Println(hypot(3, 4))
+
+	// Function closures
+
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
 
 func printSlice(s string, x []int) {
 	fmt.Printf("%s len=%d cap=%d %v\n",
 		s, len(x), cap(x), x)
+}
+
+/* Go functions may be closures.
+A closure is a function value that references variables
+from outside its body. The function may access and assign
+to the referenced variables; in this sense the function is
+"bound" to the variables.
+
+For example, the adder function returns a closure.
+Each closure is bound to its own sum variable. */
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
