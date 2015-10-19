9ac17fef277faf09fe949580f96329d468ed1244
klashxx <klashxx@gmail.com>
2015-10-20 00:20:05 +0200

    method on any type

--- methods_interfaces/methods_interfaces.go
+++ methods_interfaces/methods_interfaces.go
@@ -9,11 +9,32 @@ type Vertex struct {
 	X, Y float64
 }
 
+// Go does not have classes. However, you can
+// define methods on struct types.
+
 func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// You can declare a method on any type that is
+// declared in your package, not just struct types.
+
+// However, you cannot define a method on a type
+// from another package (including built in types).
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
 }
 
 func main() {
 	v := &Vertex{3, 4}
 	fmt.Println(v.Abs())
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
 }
