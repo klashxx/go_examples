24e589eb52feee813a43318a42b77b9c3f190d17
klashxx <klashxx@gmail.com>
2015-10-21 22:18:05 +0200

    goroutines 2

--- goroutines/goroutines.go
+++ goroutines/goroutines.go
@@ -12,7 +12,35 @@ func say(s string) {
 	}
 }
 
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum // send sum to c
+}
+
 func main() {
 	go say("world")
 	say("hello")
+
+	// Channels are a typed conduit through which you can
+	// send and receive values with the channel operator, <-
+
+	a := []int{7, 2, 8, -9, 4, 0}
+
+	// Like maps and slices, channels must be created before use
+	c := make(chan int)
+
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+
+	// The data flows in the direction of the arrow
+	x, y := <-c, <-c // receive from c
+
+	// By default, sends and receives block until the other
+	// side is ready. This allows goroutines to synchronize
+	// without explicit locks or condition variables.
+
+	fmt.Println(x, y, x+y)
 }
